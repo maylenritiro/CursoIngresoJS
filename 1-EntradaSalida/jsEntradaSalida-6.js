f046bb91dc81ec74dc32d7733ecc03de4da0057f
alumno <alumno@utnfra.com.ar>
2020-02-06 20:23:47 -0300

    ej

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -1,8 +1,16 @@
 /*Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt) y Sumarlos.
-mostrar el resulto por medio de "ALERT"*/
+mostrar el resultado por medio de "ALERT"*/
 function sumar()
 {
-
+    var numero1;
+    var numero2;
+    var suma_numeros;
+    numero1 = document.getElementById("numeroUno").value;
+    numero1 = parseInt(numero1);
+    numero2 = document.getElementById("numeroDos").value;
+    numero2 = parseInt(numero2);
+    suma_numeros = numero1+numero2;
+    alert(suma_numeros);
 }
 
