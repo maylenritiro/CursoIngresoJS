db7fea86abc08a6ab292c9564c64a6f98974abdb
alumno <alumno@utnfra.com.ar>
2020-02-06 22:16:00 -0300

    ej

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -2,23 +2,47 @@
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
+var numeroUno;
+var numeroDos;
+var resutado;
+
 function sumar()
 {	
-	
+    numeroUno = document.getElementById("numeroUno").value;
+    numeroUno = parseInt(numeroUno);
+    numeroDos = document.getElementById("numeroDos").value;
+    numeroDos = parseInt(numeroDos);
+    resultado = numeroUno + numeroDos;
+    alert(resutado);
 }
 
 function restar()
 {
-	
+    numeroUno = document.getElementById("numeroUno").value;
+    numeroUno = parseInt(numeroUno);
+    numeroDos = document.getElementById("numeroDos").value;
+    numeroDos = parseInt(numeroDos);
+    resultado = numeroUno - numeroDos;
+    alert(resutado);
 }
 
 function multiplicar()
 { 
-	
+    numeroUno = document.getElementById("numeroUno").value;
+    numeroUno = parseInt(numeroUno);
+    numeroDos = document.getElementById("numeroDos").value;
+    numeroDos = parseInt(numeroDos);
+    resultado = numeroUno * numeroDos;
+    alert(resutado);
 }
 
 function dividir()
 {
-	
+    numeroUno = document.getElementById("numeroUno").value;
+    numeroUno = parseInt(numeroUno);
+    numeroDos = document.getElementById("numeroDos").value;
+    numeroDos = parseInt(numeroDos);
+    resultado = numeroUno / numeroDos;
+    alert(resutado);
 }
 
