11e5f0bd36f4c0f4651be223121629cdf1018de6
alumno <alumno@utnfra.com.ar>
2020-02-06 22:20:24 -0300

    m

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,10 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
-
+    var importe;
+    var importe_aumentado;
+    importe = document.getElementById("sueldo").value;
+    importe_aumentado = importe * 1.1;
+    document.getElementById("Resultado").value = importe_aumentado;
 	
 }
